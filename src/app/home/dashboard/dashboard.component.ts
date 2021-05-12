72485ca7da851bd0d061ef1887a25f307fb45178
Asif <asif@uni-koblenz.de>
2021-05-12 18:34:39 +0200

    dashboard.component.ts update

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -1,9 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.less']
+  styleUrls: ['./dashboard.component.less'],
+  animations: [
+  // trigger('fadeInOut', [
+  //   transition(':enter', [
+  //     style({ opacity: 0 }),
+  //     animate('1000ms', style({ opacity: 1 })),
+  //   ]),
+  //   transition(':leave', [
+  //     animate('10ms', style({ opacity: 0 }))
+  //   ])
+  // ])
+  trigger('fadeInOut', [
+    state('void', style({
+      opacity: 0
+    })),
+    transition('void <=> *', animate(1000)),
+  ])
+]
 })
 export class DashboardComponent implements OnInit {
   title = 'weather-app';
